84899f053fb506588d415ac68a35b5cab599b12d
Paul Weidland <github@uk3k.de>
2026-06-24 13:38:53 +0200

    Update fetch-update-script.sh

--- fetch-update-script.sh
+++ fetch-update-script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#placed in home dir as update-pi.sh on clients
+#>fetch-update-script.sh< is placed in home dir as >update-pi.sh< on clients
 clear
 echo "fetching most recent version of update-script from github..."
 mkdir -p scripts
-cd scripts
-curl -O https://github.com/uk3k/raspi-overlayfs-updates/blob/main/update-pi.sh
+rm -rf scripts/raspi-overlayfs-updates
+git clone https://github.com/uk3k/raspi-overlayfs-updates.git scripts/raspi-overlayfs-updates
+cd scripts/raspi-overlayfs-updates
 chmod +x update-pi.sh
 echo "executing actual update-script now!"
 sudo -u root sh update-pi.sh
