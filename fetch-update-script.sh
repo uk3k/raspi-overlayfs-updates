7b9eac9472b94a3e2d584dbc0ce97d49616904ee
Paul Weidland <github@uk3k.de>
2026-06-24 13:22:18 +0200

    Update fetch-update-script.sh

--- fetch-update-script.sh
+++ fetch-update-script.sh
@@ -4,6 +4,6 @@ clear
 echo "fetching most recent version of update-script from github..."
 mkdir -p scripts
 cd scripts
-wget-O https://github.com/uk3k/raspi-overlayfs-updates/blob/main/update-pi.sh
+curl -O https://github.com/uk3k/raspi-overlayfs-updates/blob/main/update-pi.sh
 chmod +x update-pi.sh
 sudo -u root sh update-pi.sh
